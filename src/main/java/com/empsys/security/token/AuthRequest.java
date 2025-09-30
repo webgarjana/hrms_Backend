fa312caa9ce4a72c81e799d180c5117ce4a1ee8b
webgarjana <wgarjana@gmail.com>
2025-09-30 10:45:34 +0530

    Update AuthRequest.java

--- src/main/java/com/empsys/security/token/AuthRequest.java
+++ src/main/java/com/empsys/security/token/AuthRequest.java
@@ -1,23 +1,23 @@
-package com.empsys.security.token;
+// package com.empsys.security.token;
 
-public class AuthRequest {
-    private String username;
-    private String password;
+// public class AuthRequest {
+//     private String username;
+//     private String password;
 
-    public String getUsername() {
-        return username;
-    }
+//     public String getUsername() {
+//         return username;
+//     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+//     public void setUsername(String username) {
+//         this.username = username;
+//     }
 
-    public String getPassword() {
-        return password;
-    }
+//     public String getPassword() {
+//         return password;
+//     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-// getters & setters
-}
+//     public void setPassword(String password) {
+//         this.password = password;
+//     }
+// // getters & setters
+// }
