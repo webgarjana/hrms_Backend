65545ebf9777824abd331b4514d70299b1b4c29b
webgarjana <wgarjana@gmail.com>
2025-09-30 10:46:28 +0530

    Update AuthResponse.java

--- src/main/java/com/empsys/security/token/AuthResponse.java
+++ src/main/java/com/empsys/security/token/AuthResponse.java
@@ -1,13 +1,13 @@
-package com.empsys.security.token;
+// package com.empsys.security.token;
 
-public class AuthResponse {
-    private String token;
+// public class AuthResponse {
+//     private String token;
 
-    public AuthResponse(String token) {
-        this.token = token;
-    }
+//     public AuthResponse(String token) {
+//         this.token = token;
+//     }
 
-    public String getToken() {
-        return token;
-    }
-}
+//     public String getToken() {
+//         return token;
+//     }
+// }
